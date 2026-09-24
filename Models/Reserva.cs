81a202359abb89300de7266ad9f65a10982fb999
grandepedro87 <pedro6lucca@gmail.com>
2026-09-24 02:13:20 -0300

    Sistema de hospedagem do hotel finalizado!

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -16,15 +16,17 @@ namespace DesafioProjetoHospedagem.Models
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            // Implementado!!!
+            bool tamanho = hospedes.Count <= Suite.Capacidade;
+            if (tamanho)
             {
                 Hospedes = hospedes;
             }
             else
             {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                // Implementado!!!
+                throw new ArgumentException("Numero de hospedes passou o permitido");
             }
         }
 
@@ -36,22 +38,24 @@ namespace DesafioProjetoHospedagem.Models
         public int ObterQuantidadeHospedes()
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            // Implementado!!!
+            return Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
         {
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            // Implementado!!
+            decimal valor = DiasReservados * Suite.ValorDiaria;
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            // Implementado!!!
+            bool desconto = DiasReservados >= 10;
+            if (desconto)
             {
-                valor = 0;
+                decimal porcentagem = 10;
+                valor = valor - (valor * porcentagem / 100);
             }
 
             return valor;
